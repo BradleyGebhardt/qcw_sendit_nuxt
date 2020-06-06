b4a68608c59bd6456f71fcf0cfd9987a4c93c1cd
BradleyGebhardt <39223742+BradleyGebhardt@users.noreply.github.com>
2020-06-06 23:37:20 +0200

    Cleaned up the boilerplate and added auto-component imports

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'QCW Sendit',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -43,7 +43,9 @@ export default {
     '@nuxtjs/axios',
     '@nuxtjs/pwa',
     // Doc: https://github.com/nuxt-community/dotenv-module
-    '@nuxtjs/dotenv'
+    '@nuxtjs/dotenv',
+    // Doc: https://github.com/nuxt/components
+    '@nuxt/components'
   ],
   /*
    ** Axios module configuration
