e5950285e360c70148f8b283b660a96446634dca
BradleyGebhardt <39223742+BradleyGebhardt@users.noreply.github.com>
2020-06-07 13:30:45 +0200

    Added login and signup links

--- components/s-nav-drawer.vue
+++ components/s-nav-drawer.vue
@@ -5,10 +5,22 @@
 			@click="emitModel()"
 		></div>
 		<div :class="{ side: true, side__expanded: expanded }">
+			<h1>Navigation</h1>
+			<hr class="nav" />
 			<ul>
 				<li>
 					<nuxt-link to="/">
 						Home
+					</nuxt-link>
+				</li>
+				<li>
+					<nuxt-link to="/login">
+						Login
+					</nuxt-link>
+				</li>
+				<li>
+					<nuxt-link to="/signup">
+						Signup
 					</nuxt-link>
 				</li>
 			</ul>
@@ -58,6 +70,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$side-width: 250px;
+
 .overlay {
 	position: absolute;
 	// display: block;
@@ -67,7 +81,7 @@ export default {
 	height: 100vh;
 
 	&__expanded {
-		right: 200px;
+		right: $side-width;
 	}
 }
 
@@ -78,13 +92,28 @@ export default {
 	height: 100vh;
 	background-color: #000000d2;
 	color: white;
-	width: 200px;
+	width: $side-width;
 	transform: scaleX(0);
 	transform-origin: 100% 0%;
 	transition: all 0.3s ease;
 
+	h1 {
+		font-weight: 100;
+		text-align: center;
+	}
+
 	ul {
 		list-style: none;
+		padding-top: 60px;
+
+		li {
+			text-align: center;
+			color: white;
+		}
+
+		li:nth-of-type(even) {
+			margin: 15px 0;
+		}
 	}
 
 	&__expanded {
