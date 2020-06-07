8505a8375c6f02b93ec2e074e8e4f0c0110a8585
BradleyGebhardt <39223742+BradleyGebhardt@users.noreply.github.com>
2020-06-07 17:03:57 +0200

    Disabled all styles on the signup page

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,13 +1,37 @@
 <template>
-	<div>
-		Signup
+	<div class="wrapper">
+		<form @submit.prevent>
+			<s-text-field v-model="user.email" label="Email" />
+			{{ user.email }}
+		</form>
 	</div>
 </template>
 
 <script>
 export default {
 	name: 'SignupPage',
+
+	data() {
+		return {
+			user: {
+				email: '',
+			},
+		}
+	},
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+// @media screen and (max-width: 768px) {
+// 	.wrapper {
+// 		background-color: #564d65;
+// 		height: 100%;
+// 		margin: -50px 0;
+// 		padding-top: 50px;
+
+// 		form {
+// 			padding-top: 50px;
+// 		}
+// 	}
+// }
+</style>
