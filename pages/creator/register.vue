51d1401c42ab875f10e4a6605897c7249058e4bb
BradleyGebhardt <39223742+BradleyGebhardt@users.noreply.github.com>
2020-06-08 21:51:23 +0200

    Added the creator regisrtation page

---
+++ pages/creator/register.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="wrapper">
+		<h2>
+			Creator registration
+		</h2>
+	</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+	color: white;
+}
+</style>
