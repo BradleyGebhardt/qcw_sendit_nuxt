aec1b20852e5327bb8d887584cf3b6e4897ee820
BradleyGebhardt <39223742+BradleyGebhardt@users.noreply.github.com>
2020-06-08 21:50:40 +0200

    Added the creator index page

---
+++ pages/creator/index.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="wrapper">
+		<h1>
+			Creator index page
+		</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'CreatorPage',
+}
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+	color: white;
+}
+</style>
